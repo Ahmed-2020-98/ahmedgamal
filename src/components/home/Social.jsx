66549cab5c7b7f2c196f5d1c2fea1eef561da247
Ahmed-2020-98 <ahmedgamalmansour11@gmail.com>
2022-09-27 09:43:39 +0200

    1hrmin10

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -6,6 +6,9 @@ const Social = () => {
         <a href='https://www.instagram.com/ahmed_gamal.jr/' className='home__social-icon' target='_blank'>
             <i class="uil uil-instagram"></i>
         </a>
+        <a href='tel:01559166164' className='home__social-icon' target='_blank'>
+            <i class="uil uil-whatsapp"></i>
+        </a>
         <a href='https://www.linkedin.com/in/ahmed-gamal-800921192/' className='home__social-icon' target='_blank'>
         <i class="uil uil-linkedin"></i>
         </a>
